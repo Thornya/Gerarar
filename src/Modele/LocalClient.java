3468e06497a77955a1624f3432b8af62f8eaa005
EwaldJa <ewald.janin@gmail.com>
2019-05-13 11:49:48 +0200

    Added errorcodes clientside

--- src/Modele/LocalClient.java
+++ src/Modele/LocalClient.java
@@ -8,7 +8,7 @@ public class LocalClient {
     public static final int max_trial_transfert = 3;
     public static final int wait_time_transfert_ms = 5000;
 
-    public static final int transfer_succesfull = 0;
+    public static final int transfer_successful = 0;
 
     public static final int error_unavailable_server = 10;
     public static final int error_no_server_response = 20;
@@ -19,7 +19,7 @@ public class LocalClient {
     public static final int error_server_file_not_found = 110;
     public static final int error_server_access_violation = 120;
     public static final int error_server_disk_full = 130;
-    public static final int error_server_illegal_tftp_opereation = 140;
+    public static final int error_server_illegal_tftp_operation = 140;
     public static final int error_server_unknown_transfer_id = 150;
     public static final int error_server_file_already_exists = 160;
     public static final int error_server_unkown_user = 170;
@@ -27,6 +27,15 @@ public class LocalClient {
     public static final int error_file_creation = - 10;
     public static final int error_file_writing = -20;
     public static final int error_creating_socket = - 30;
+
+    public static final int error_client_undefined = -100;
+    public static final int error_client_file_not_found = -110;
+    public static final int error_client_access_violation = -120;
+    public static final int error_client_disk_full = -130;
+    public static final int error_client_illegal_tftp_operation = -140;
+    public static final int error_client_unknown_transfer_id = -150;
+    public static final int error_client_file_already_exists = -160;
+    public static final int error_client_unkown_user = -170;
 
     private InetAddress server_address;
     private int server_port;
@@ -56,7 +65,7 @@ public class LocalClient {
             //TODO handle the exception
         }
         server_port = Integer.parseInt(server_port_str);
-        return transfer_succesfull;
+        return transfer_successful;
     }
 
     public int SendFile(String server_address_str, String server_port_str, String filename) {
@@ -70,7 +79,7 @@ public class LocalClient {
             //TODO handle the exception
         }
         server_port = Integer.parseInt(server_port_str);
-        return transfer_succesfull;
+        return transfer_successful;
     }
 
     private void sendRequest(boolean requestMode, String filename_str) {
