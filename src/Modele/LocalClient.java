78bce30bb0e246cfe3e9a75665ad70e085f6033d
CapitainePigeon <37774136+CapitainePigeon@users.noreply.github.com>
2019-05-13 10:54:49 +0200

    code erreur

--- src/Modele/LocalClient.java
+++ src/Modele/LocalClient.java
@@ -1,4 +1,24 @@
 package Modele;
+int max_trial_transfert = 3;
+int wait_time_transfert_ms = 5000;
+
+int error_unavailable_server = 10;
+int error_no_server_response = 20;
+int error_sending = 30;
+int error_receiving = 40;
+
+int error_server_undefined = 100;
+int error_server_file_not_found = 110;
+int error_server_access_violation = 120;
+int error_server_disk_full = 130;
+int error_server_illegal_tftp_opereation = 140;
+int error_server_unknown_transfer_id = 150;
+int error_server_file_already_exists = 160;
+int error_server_unkown_user = 170;
+
+int error_file_creation = - 10;
+int error_file_writing = -20;
+int error_creating_socket = - 30;
 
 public class LocalClient {
 }
